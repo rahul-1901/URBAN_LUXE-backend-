d455c0a53bb272028fcd97f2764640deb919e0f8
Rahul <147478589+iitian12@users.noreply.github.com>
2025-03-03 00:35:18 +0530

    uninstalledBcrypt

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/connectDB.js';
-import clothesModel from './models/clothes.model.js';
 import route from './routes/clothes.routes.js';
 import userRoute from './routes/user.routes.js';
-import bcrypt from 'bcrypt';
 import deliveryRoute from './routes/delivery.routes.js';
 const app = express();
 dotenv.config();
@@ -18,10 +16,6 @@ app.use(express.json());
 app.use("/api/clothes", route);
 app.use("/api/user", userRoute);
 app.use("/api", deliveryRoute);
-
-app.get("/ro", (req, res) => {
-    res.send("Welcome to the E-commerce API!");
-});
 
 const PORT = process.env.PORT || 3000;
 connectDB().then(() => {
