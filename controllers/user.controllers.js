8fdd15598d88e5c33d5400837edbc0b44c421692
Rahul <147478589+iitian12@users.noreply.github.com>
2025-03-03 00:39:40 +0530

    solvingIssue 122

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,5 +1,5 @@
 import UserModel from "../models/user.model.js";
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import clothesModel from "../models/clothes.model.js";
