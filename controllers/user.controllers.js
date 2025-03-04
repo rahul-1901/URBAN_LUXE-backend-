8da559ad250d457440fa756736b2d87bbd1a7f35
Rahul <147478589+iitian12@users.noreply.github.com>
2025-03-04 23:28:50 +0530

    changedSomething

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -178,7 +178,7 @@ export const userDetails = async (req, res) => {
         const user = await UserModel.findOne({
             email
         })
-
+  
         if(!user) {
             return res.status(403).json({
                 message: "User not found..."
