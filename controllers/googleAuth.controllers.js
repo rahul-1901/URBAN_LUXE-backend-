34c0781836139d4a11478b9af3b344c8daab9e86
Rahul <147478589+iitian12@users.noreply.github.com>
2025-03-11 19:59:43 +0530

    envChnages

--- controllers/googleAuth.controllers.js
+++ controllers/googleAuth.controllers.js
@@ -9,7 +9,7 @@ export const googleAuth = async (req, res) => {
         const googleRes = await oauth2client.getToken(code);
         oauth2client.setCredentials(googleRes.tokens);
 
-        const userRes = await axios.get(
+        const userRes = await axios.get (
             `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`
         )
 
